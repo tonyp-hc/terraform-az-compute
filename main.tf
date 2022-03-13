a756a6c4c8000c5e6f9014ddde933844e3af651e
Tony Pulickal <ttp@hashicorp.com>
2022-03-13 14:23:58 -0400

    disable ssh

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "linuxservers" {
   nb_instances                      = var.nb_instances
   vnet_subnet_id                    = data.terraform_remote_state.vnet.outputs.az_network_subnets[0]
   delete_os_disk_on_termination     = var.delete_disk
-
+  enable_ssh_key                    = false
   tags = {
     environment = var.env
     owner       = var.owner
