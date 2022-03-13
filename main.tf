466915651c2d98482a1ce51c160f693411f0df6c
Tony Pulickal <ttp@hashicorp.com>
2022-03-13 14:14:19 -0400

    fix disk params

--- main.tf
+++ main.tf
@@ -12,19 +12,20 @@ provider "azurerm" {
 }
 
 module "linuxservers" {
-  source                        = "app.terraform.io/TonyPulickal/compute/azurerm"
-  resource_group_name           = "${var.prefix}-rg"
-  vm_hostname                   = var.vm_hostname
-  nb_public_ip                  = "1"
-  public_ip_dns                 = ["tfelinuxdemo"]
-  remote_port                   = var.remote_port
-  nb_instances                  = var.nb_instances
-  vnet_subnet_id                = data.terraform_remote_state.vnet.outputs.az_network_subnets[0]
-  delete_os_disk_on_termination = var.delete_disk
-  data_disk                     = "true"
-  data_disk_size_gb             = var.data_disk_size
-  data_sa_type                  = var.data_sa_type
-  ssh_key                       = var.ssh_public_key
+  source                            = "app.terraform.io/TonyPulickal/compute/azurerm"
+  resource_group_name               = "${var.prefix}-rg"
+  vm_hostname                       = var.vm_hostname
+  nb_public_ip                      = "1"
+  public_ip_dns                     = ["tfelinuxdemo"]
+  remote_port                       = var.remote_port
+  nb_instances                      = var.nb_instances
+  vnet_subnet_id                    = data.terraform_remote_state.vnet.outputs.az_network_subnets[0]
+  delete_os_disk_on_termination     = var.delete_disk
+  delete_data_disks_on_termination  = var.delete_disk 
+  nb_data_disk                      = 1 
+  data_disk_size_gb                 = var.data_disk_size
+  data_sa_type                      = var.data_sa_type
+  ssh_key                           = var.ssh_public_key
 
   tags = {
     environment = var.env
